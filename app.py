d283bb06779c4c1c57def187aca10a702b7a560b
JackyDom <sopheaoudom8@gmail.com>
2025-01-29 14:47:09 +0700

    update

--- app.py
+++ app.py
@@ -84,4 +84,5 @@ def cafe():
 
 
 if __name__ == '__main__':
-    app.run(debug=True,host='0.0.0.0', port=35174)
+    port = int(os.environ.get("PORT", 35174))
+    app.run(debug=True,host='0.0.0.0', port=port)
